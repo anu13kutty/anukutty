0dd018b2743032a59866568dfaef42a3a75e347e
anu13kutty <48052889+anu13kutty@users.noreply.github.com>
2019-04-05 11:19:55 +0530

    Create table.c

---
+++ table.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        a=n*i;
+        printf(" %d",a);
+    }
+    
+}
